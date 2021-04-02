12718afba2460d056a721aabb0ac1a8414939054
Christof Weigl <christof.weigl@continental-corporation.com>
2021-04-02 13:11:37 +0200

    try combo

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
+// This is based on the oversized volume knob project of Mikolas Zuza from Prusaprinters.org
+// Find the project here: https://blog.prusaprinters.org/3d-print-an-oversized-media-control-volume-knob-arduino-basics_30184/
 // Read the full tutorial at prusaprinters.org
 
 #include <ClickEncoder.h>
 #include <TimerOne.h>
 #include <HID-Project.h>
+// for mute button w/ led
+// arduino-libraries/Keyboard
 
 #define ENCODER_CLK A0 // Change A0 to, for example, A5 if you want to use analog pin 5 instead
 #define ENCODER_DT A1
@@ -10,6 +14,24 @@
 
 ClickEncoder *encoder; // variable representing the rotary encoder
 int16_t last, value; // variables for current and last rotation value
+
+
+// ---- for mute button w/ led
+#define BUTTON_PIN A8
+#define LED_PIN A6
+
+// constants won't change. They're used here to set pin numbers:
+const int DEBOUNCE_DELAY = 50;   // the debounce time; increase if the output flickers
+// Variables will change:
+int lastSteadyState = LOW;       // the previous steady state from the input pin
+int lastFlickerableState = LOW;  // the previous flickerable state from the input pin
+int currentState;                // the current reading from the input pin
+int currentLedState;
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+
 
 void timerIsr() {
   encoder->service();
@@ -23,9 +45,13 @@ void setup() {
   Timer1.initialize(1000); // Initializes the timer, which the rotary encoder uses to detect rotation
   Timer1.attachInterrupt(timerIsr); 
   last = -1;
+
+  // for mute button w/ led
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
 } 
 
-void loop() {  
+void loop() {
   value += encoder->getValue();
 
   // This part of the code is responsible for the actions when you rotate the encoder
@@ -57,6 +83,60 @@ void loop() {
 
   delay(10); // Wait 10 milliseconds, we definitely don't need to detect the rotary encoder any faster than that
   // The end of the loop() function, it will start again from the beggining (the begginging of the loop function, not the whole document)
+
+  // for mute button w/ led
+  // read the state of the switch/button:
+  currentState = digitalRead(BUTTON_PIN);
+  currentLedState = digitalRead(LED_PIN);
+
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH), and you've waited long enough
+  // since the last press to ignore any noise:
+
+  // If the switch/button changed, due to noise or pressing:
+  if (currentState != lastFlickerableState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+    // save the the last flickerable state
+    lastFlickerableState = currentState;
+  }
+
+  //digitalWrite(LED_PIN, HIGH);
+  //delay(1000); 
+  //digitalWrite(LED_PIN, LOW);
+  //delay(1000); 
+
+  if ((millis() - lastDebounceTime) > DEBOUNCE_DELAY) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if(lastSteadyState == HIGH && currentState == LOW) {
+
+
+      if (currentLedState == HIGH) {
+        digitalWrite(LED_PIN, LOW);
+      }
+      else {
+        digitalWrite(LED_PIN, HIGH);
+      }
+
+      Keyboard.press(KEY_LEFT_GUI);
+      Keyboard.press('1');
+      delay(100);
+      Keyboard.releaseAll();
+      delay(100);
+      // Keyboard.println("You pressed the button ");
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.press(KEY_LEFT_SHIFT);
+      Keyboard.press('m');
+      delay(100);
+      Keyboard.releaseAll();
+    }
+    // save the the last steady state
+    lastSteadyState = currentState;
+  }
+
 }
 
 
